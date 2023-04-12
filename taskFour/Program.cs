30b7c9be6115b974ae869f4c1b2689f5b8ea8c8b
Alexander <firefighter-911@rambler.ru>
2023-04-12 22:26:08 +0300

    Решение четвертой задачи

---
+++ taskFour/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+Console.Write("Введите второе число: ");
+string userInput2 = Console.ReadLine() ?? "";
+Console.Write("Введите третье число: ");
+string userInput3 = Console.ReadLine() ?? "";
+int userNumberA = int.Parse(userInput1);
+int userNumberB = int.Parse(userInput2);
+int userNumberС = int.Parse(userInput3);
+
+int max = userNumberA;
+
+if (userNumberB > max) max = userNumberB;
+
+if (userNumberС > max) max = userNumberС;
+
+Console.WriteLine($"Максимальное число -> {max}");
