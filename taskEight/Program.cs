23019ae931ddf9db713df19126925aeddf1571d5
Alexander <firefighter-911@rambler.ru>
2023-04-13 00:29:51 +0300

    Решение восьмой задачи

---
+++ taskEight/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+
+int index = 1;
+
+while (index <= userNumber)
+{
+    if (index % 2 == 0)
+    {
+        Console.Write(index+",");
+    }
+
+    index++;
+}
+
