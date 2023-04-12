24046c779ab811d2266e65be4111dd00b156ae0d
Alexander <firefighter-911@rambler.ru>
2023-04-12 22:00:52 +0300

    Решение второй задачи

---
+++ taskTwo/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+Console.Write("Введите второе число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int userNumderA = int.Parse (userInput1);
+int userNumderB = int.Parse (userInput2);
+
+int max = userNumderA;
+
+if (userNumderB > max) max = userNumderB;
+
+Console.WriteLine($"max = {max}");
