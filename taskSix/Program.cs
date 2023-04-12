78d72d291cd821046a75f1c149e9b61154f64e45
Alexander <firefighter-911@rambler.ru>
2023-04-12 22:35:14 +0300

    Решение шестой задачи

---
+++ taskSix/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+
+if (userNumber %2 == 0)
+{
+    Console.WriteLine($"{userNumber} -> Да");
+}
+else
+{
+    Console.WriteLine($"{userNumber} -> Нет");
+}
+
+
